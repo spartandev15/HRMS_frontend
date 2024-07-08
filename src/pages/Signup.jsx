0bbac76bab9f6360051dfc7c426237d3a80a8f8a
mohitspartanbots <mohit@spartanbots.com>
2024-07-08 18:20:48 +0530

    fixes in signup ui

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -56,60 +56,31 @@ const Signup = () => {
       {error && <p style={{ color: "red" }}>{error}</p>}
       {successMessage && <p style={{ color: "green" }}>{successMessage}</p>}
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="first_name">First Name</label>
-          <input
-            type="text"
-            id="first_name"
-            name="first_name"
-            value={formData.first_name}
-            onChange={handleChange}
-            required
-          />
+        <div className="d-flex p-0">
+          <div className="w-50">
+            <label htmlFor="first_name">First Name</label>
+            <input
+              type="text"
+              id="first_name"
+              name="first_name"
+              value={formData.first_name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="w-50">
+            <label htmlFor="last_name">Last Name</label>
+            <input
+              type="text"
+              id="last_name"
+              name="last_name"
+              value={formData.last_name}
+              onChange={handleChange}
+              required
+            />
+          </div>
         </div>
-        <div>
-          <label htmlFor="last_name">Last Name</label>
-          <input
-            type="text"
-            id="last_name"
-            name="last_name"
-            value={formData.last_name}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="organisation">Organisation</label>
-          <input
-            type="text"
-            id="organisation"
-            name="organisation"
-            value={formData.organisation}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="organisation_id">Organisation ID</label>
-          <input
-            type="text"
-            id="organisation_id"
-            name="organisation_id"
-            value={formData.organisation_id}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="address">Address</label>
-          <textarea
-            id="address"
-            name="address"
-            value={formData.address}
-            onChange={handleChange}
-            required
-          />
-        </div>
+
         <div>
           <label htmlFor="email">Email</label>
           <input
@@ -121,30 +92,74 @@ const Signup = () => {
             required
           />
         </div>
+
+        <div className="d-flex p-0">
+          <div className="w-50">
+            <label htmlFor="organisation">Organisation</label>
+            <input
+              type="text"
+              id="organisation"
+              name="organisation"
+              value={formData.organisation}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="w-50">
+            <label htmlFor="organisation_id">Organisation ID</label>
+            <input
+              type="text"
+              id="organisation_id"
+              name="organisation_id"
+              value={formData.organisation_id}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        </div>
+
         <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
+          <label htmlFor="address">Address</label>
+          <textarea
+            id="address"
+            name="address"
+            value={formData.address}
             onChange={handleChange}
             required
           />
         </div>
-        <div>
-          <label htmlFor="confirm_password">Confirm Password</label>
-          <input
-            type="password"
-            id="confirm_password"
-            name="confirm_password"
-            value={formData.confirm_password}
-            onChange={handleChange}
-            required
-          />
+
+        <div className="d-flex p-0">
+          <div className="w-50">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="w-50">
+            <label htmlFor="confirm_password">Confirm Password</label>
+            <input
+              type="password"
+              id="confirm_password"
+              name="confirm_password"
+              value={formData.confirm_password}
+              onChange={handleChange}
+              required
+            />
+          </div>
         </div>
-        <button type="submit">Signup</button>
-        <button onClick={() => navigate("/login")}>Login</button>
+
+        <div>
+          <button type="submit">Signup</button>
+        </div>
+        <div>
+          <button onClick={() => navigate("/login")}>Login</button>
+        </div>
       </form>
     </div>
   );
